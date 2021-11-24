b6c96abced0ae9e5c7218937f7f89aa7a769dc2d
Flood123789 <73972594+Flood123789@users.noreply.github.com>
2021-11-24 16:35:38 -0500

    Selection + Lbl

--- Final Project/Main_Page.Designer.cs
+++ Final Project/Main_Page.Designer.cs
@@ -32,31 +32,32 @@ namespace Final_Project
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.Gen_Hist_Tb = new System.Windows.Forms.TabPage();
             this.Undo_btn_F1 = new System.Windows.Forms.Button();
             this.Select_Pt = new System.Windows.Forms.Button();
-            this.Save_Button_F1 = new System.Windows.Forms.Button();
+            this.Save_Button_Main = new System.Windows.Forms.Button();
             this.Select_box = new System.Windows.Forms.DataGridView();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.patientIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.selectionBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.medsDataSet = new Final_Project.MedsDataSet();
+            this.Gen_Hist_Tb = new System.Windows.Forms.TabPage();
+            this.Save_Gen = new System.Windows.Forms.Button();
+            this.GenMedDataView = new System.Windows.Forms.DataGridView();
+            this.patientIDDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.generalMedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.genMedBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.selectionTableAdapter = new Final_Project.MedsDataSetTableAdapters.SelectionTableAdapter();
             this.genMedTableAdapter = new Final_Project.MedsDataSetTableAdapters.GenMedTableAdapter();
-            this.Save_Gen = new System.Windows.Forms.Button();
-            this.patientIDDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.generalMedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Patient_IdLbl = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.Gen_Hist_Tb.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Select_box)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.selectionBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.medsDataSet)).BeginInit();
+            this.Gen_Hist_Tb.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GenMedDataView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.genMedBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
@@ -67,34 +68,22 @@ namespace Final_Project
             this.tabControl1.Location = new System.Drawing.Point(6, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(782, 434);
+            this.tabControl1.Size = new System.Drawing.Size(711, 434);
             this.tabControl1.TabIndex = 5;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.Undo_btn_F1);
             this.tabPage1.Controls.Add(this.Select_Pt);
-            this.tabPage1.Controls.Add(this.Save_Button_F1);
+            this.tabPage1.Controls.Add(this.Save_Button_Main);
             this.tabPage1.Controls.Add(this.Select_box);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(774, 408);
+            this.tabPage1.Size = new System.Drawing.Size(703, 408);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Select";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // Gen_Hist_Tb
-            // 
-            this.Gen_Hist_Tb.Controls.Add(this.Save_Gen);
-            this.Gen_Hist_Tb.Controls.Add(this.dataGridView1);
-            this.Gen_Hist_Tb.Location = new System.Drawing.Point(4, 22);
-            this.Gen_Hist_Tb.Name = "Gen_Hist_Tb";
-            this.Gen_Hist_Tb.Padding = new System.Windows.Forms.Padding(3);
-            this.Gen_Hist_Tb.Size = new System.Drawing.Size(774, 408);
-            this.Gen_Hist_Tb.TabIndex = 1;
-            this.Gen_Hist_Tb.Text = "General Medical History";
-            this.Gen_Hist_Tb.UseVisualStyleBackColor = true;
             // 
             // Undo_btn_F1
             // 
@@ -113,15 +102,17 @@ namespace Final_Project
             this.Select_Pt.TabIndex = 7;
             this.Select_Pt.Text = "Select";
             this.Select_Pt.UseVisualStyleBackColor = true;
+            this.Select_Pt.Click += new System.EventHandler(this.Select_Pt_Click_1);
             // 
-            // Save_Button_F1
+            // Save_Button_Main
             // 
-            this.Save_Button_F1.Location = new System.Drawing.Point(214, 177);
-            this.Save_Button_F1.Name = "Save_Button_F1";
-            this.Save_Button_F1.Size = new System.Drawing.Size(75, 23);
-            this.Save_Button_F1.TabIndex = 6;
-            this.Save_Button_F1.Text = "Save";
-            this.Save_Button_F1.UseVisualStyleBackColor = true;
+            this.Save_Button_Main.Location = new System.Drawing.Point(214, 177);
+            this.Save_Button_Main.Name = "Save_Button_Main";
+            this.Save_Button_Main.Size = new System.Drawing.Size(75, 23);
+            this.Save_Button_Main.TabIndex = 6;
+            this.Save_Button_Main.Text = "Save";
+            this.Save_Button_Main.UseVisualStyleBackColor = true;
+            this.Save_Button_Main.Click += new System.EventHandler(this.Save_Button_F1_Click);
             // 
             // Select_box
             // 
@@ -136,20 +127,6 @@ namespace Final_Project
             this.Select_box.Name = "Select_box";
             this.Select_box.Size = new System.Drawing.Size(346, 150);
             this.Select_box.TabIndex = 5;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.iDDataGridViewTextBoxColumn,
-            this.patientIDDataGridViewTextBoxColumn1,
-            this.generalMedDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.genMedBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(198, 137);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(361, 150);
-            this.dataGridView1.TabIndex = 0;
             // 
             // lastNameDataGridViewTextBoxColumn
             // 
@@ -179,6 +156,55 @@ namespace Final_Project
             this.medsDataSet.DataSetName = "MedsDataSet";
             this.medsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
+            // Gen_Hist_Tb
+            // 
+            this.Gen_Hist_Tb.Controls.Add(this.richTextBox1);
+            this.Gen_Hist_Tb.Controls.Add(this.Patient_IdLbl);
+            this.Gen_Hist_Tb.Controls.Add(this.Save_Gen);
+            this.Gen_Hist_Tb.Controls.Add(this.GenMedDataView);
+            this.Gen_Hist_Tb.Location = new System.Drawing.Point(4, 22);
+            this.Gen_Hist_Tb.Name = "Gen_Hist_Tb";
+            this.Gen_Hist_Tb.Padding = new System.Windows.Forms.Padding(3);
+            this.Gen_Hist_Tb.Size = new System.Drawing.Size(703, 408);
+            this.Gen_Hist_Tb.TabIndex = 1;
+            this.Gen_Hist_Tb.Text = "General Medical History";
+            this.Gen_Hist_Tb.UseVisualStyleBackColor = true;
+            // 
+            // Save_Gen
+            // 
+            this.Save_Gen.Location = new System.Drawing.Point(444, 293);
+            this.Save_Gen.Name = "Save_Gen";
+            this.Save_Gen.Size = new System.Drawing.Size(75, 23);
+            this.Save_Gen.TabIndex = 7;
+            this.Save_Gen.Text = "Save";
+            this.Save_Gen.UseVisualStyleBackColor = true;
+            this.Save_Gen.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // GenMedDataView
+            // 
+            this.GenMedDataView.AutoGenerateColumns = false;
+            this.GenMedDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GenMedDataView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.patientIDDataGridViewTextBoxColumn1,
+            this.generalMedDataGridViewTextBoxColumn});
+            this.GenMedDataView.DataSource = this.genMedBindingSource;
+            this.GenMedDataView.Location = new System.Drawing.Point(25, 33);
+            this.GenMedDataView.Name = "GenMedDataView";
+            this.GenMedDataView.Size = new System.Drawing.Size(247, 150);
+            this.GenMedDataView.TabIndex = 0;
+            // 
+            // patientIDDataGridViewTextBoxColumn1
+            // 
+            this.patientIDDataGridViewTextBoxColumn1.DataPropertyName = "Patient_ID";
+            this.patientIDDataGridViewTextBoxColumn1.HeaderText = "Patient_ID";
+            this.patientIDDataGridViewTextBoxColumn1.Name = "patientIDDataGridViewTextBoxColumn1";
+            // 
+            // generalMedDataGridViewTextBoxColumn
+            // 
+            this.generalMedDataGridViewTextBoxColumn.DataPropertyName = "General_Med";
+            this.generalMedDataGridViewTextBoxColumn.HeaderText = "General_Med";
+            this.generalMedDataGridViewTextBoxColumn.Name = "generalMedDataGridViewTextBoxColumn";
+            // 
             // genMedBindingSource
             // 
             this.genMedBindingSource.DataMember = "GenMed";
@@ -192,33 +218,22 @@ namespace Final_Project
             // 
             this.genMedTableAdapter.ClearBeforeFill = true;
             // 
-            // Save_Gen
+            // Patient_IdLbl
             // 
-            this.Save_Gen.Location = new System.Drawing.Point(444, 293);
-            this.Save_Gen.Name = "Save_Gen";
-            this.Save_Gen.Size = new System.Drawing.Size(75, 23);
-            this.Save_Gen.TabIndex = 7;
-            this.Save_Gen.Text = "Save";
-            this.Save_Gen.UseVisualStyleBackColor = true;
-            this.Save_Gen.Click += new System.EventHandler(this.button1_Click_1);
+            this.Patient_IdLbl.AutoSize = true;
+            this.Patient_IdLbl.Location = new System.Drawing.Point(25, 202);
+            this.Patient_IdLbl.Name = "Patient_IdLbl";
+            this.Patient_IdLbl.Size = new System.Drawing.Size(25, 13);
+            this.Patient_IdLbl.TabIndex = 8;
+            this.Patient_IdLbl.Text = "Null";
             // 
-            // patientIDDataGridViewTextBoxColumn1
+            // richTextBox1
             // 
-            this.patientIDDataGridViewTextBoxColumn1.DataPropertyName = "Patient_ID";
-            this.patientIDDataGridViewTextBoxColumn1.HeaderText = "Patient_ID";
-            this.patientIDDataGridViewTextBoxColumn1.Name = "patientIDDataGridViewTextBoxColumn1";
-            // 
-            // iDDataGridViewTextBoxColumn
-            // 
-            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
-            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
-            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
-            // 
-            // generalMedDataGridViewTextBoxColumn
-            // 
-            this.generalMedDataGridViewTextBoxColumn.DataPropertyName = "General_Med";
-            this.generalMedDataGridViewTextBoxColumn.HeaderText = "General_Med";
-            this.generalMedDataGridViewTextBoxColumn.Name = "generalMedDataGridViewTextBoxColumn";
+            this.richTextBox1.Location = new System.Drawing.Point(25, 220);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(388, 96);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
             // 
             // Main_Page
             // 
@@ -232,11 +247,12 @@ namespace Final_Project
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.Gen_Hist_Tb.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Select_box)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.selectionBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.medsDataSet)).EndInit();
+            this.Gen_Hist_Tb.ResumeLayout(false);
+            this.Gen_Hist_Tb.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GenMedDataView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.genMedBindingSource)).EndInit();
             this.ResumeLayout(false);
 
@@ -250,19 +266,20 @@ namespace Final_Project
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Button Undo_btn_F1;
         private System.Windows.Forms.Button Select_Pt;
-        private System.Windows.Forms.Button Save_Button_F1;
+        private System.Windows.Forms.Button Save_Button_Main;
         private System.Windows.Forms.DataGridView Select_box;
         private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn patientIDDataGridViewTextBoxColumn;
         private System.Windows.Forms.TabPage Gen_Hist_Tb;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView GenMedDataView;
         private System.Windows.Forms.BindingSource genMedBindingSource;
         private MedsDataSetTableAdapters.GenMedTableAdapter genMedTableAdapter;
         private System.Windows.Forms.Button Save_Gen;
-        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn patientIDDataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn generalMedDataGridViewTextBoxColumn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label Patient_IdLbl;
     }
 }
 
--- Final Project/Main_Page.cs
+++ Final Project/Main_Page.cs
@@ -28,6 +28,7 @@ namespace Final_Project
             this.genMedTableAdapter.Fill(this.medsDataSet.GenMed);
             // TODO: This line of code loads data into the 'medsDataSet.Selection' table. You can move, or remove it, as needed.
             this.selectionTableAdapter.Fill(this.medsDataSet.Selection);
+            this.GenMedDataView.ClearSelection();
             //  this.Gen_Hist_Tb.Enabled = false;
             //Patient_Selection.Items.AddRange(MedsDataSet.SelectionDataTable)
             //dataGridView1.Visible = false;
@@ -74,6 +75,52 @@ namespace Final_Project
             {
                 //Will save to DataBase 
                 this.genMedTableAdapter.Update(this.medsDataSet.GenMed);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(Convert.ToString(error.Message));
+            }
+        }
+
+        private void Select_Pt_Click_1(object sender, EventArgs e)
+        {
+            //Select any cell in the row and it will auto select the ID of the patient 
+            int RowIndex = Select_box.CurrentCell.RowIndex;
+
+            // This is a proof of concept to pick what ID is selected to bring up information for the Patient on other pages.
+            MessageBox.Show(Convert.ToString(Select_box.Rows[RowIndex].Cells[2].Value)); // messagebox for debug
+            var patient = (Convert.ToString(Select_box.Rows[RowIndex].Cells[2].Value));
+
+            try
+            {
+                GenMedDataView.ClearSelection();
+                foreach (DataGridViewRow row in GenMedDataView.Rows)
+                {
+                    if (row.Cells[0].Value.ToString().Equals(patient))
+                    {
+                        RowIndex = row.Index;
+                        //MessageBox.Show(Convert.ToString(RowIndex));
+                        GenMedDataView.Rows[RowIndex].Selected = true;
+                        //GenMedDataView.CurrentCell = GenMedDataView[RowIndex, 1];
+                        break;
+                    }
+                }
+                Patient_IdLbl.Text = "Patient: " + patient;
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(Convert.ToString(error.Message));
+            }
+            
+            
+        }
+
+        private void Save_Button_F1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Will save to DataBase 
+                this.selectionTableAdapter.Update(this.medsDataSet.Selection);
             }
             catch (Exception error)
             {
